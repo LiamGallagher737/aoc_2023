44f99c36efbeb6baa4c6de4ef894dbb0a35d82bd
Liam Gallagher <ljpgallagher@xtra.co.nz>
2023-12-10 11:48:47 +1300

    Day 9

---
+++ day_9/src/bin/part_2.rs
@@ -0,0 +1,36 @@
+fn main() {
+    let result = solution(include_str!("input.txt"));
+    println!("Result: {result}");
+}
+
+fn solution(input: &str) -> isize {
+    let mut histories = input
+        .lines()
+        .map(|line| {
+            line.split_whitespace()
+                .map(str::parse::<isize>)
+                .map(Result::unwrap)
+                .collect::<Vec<_>>()
+        })
+        .collect::<Vec<_>>();
+
+    histories
+        .drain(..)
+        .map(|mut history| calc_steps(vec![history.drain(..).rev().collect()]))
+        .map(|steps| steps.iter().map(|v| v.last()).map(Option::unwrap).sum::<isize>())
+        .sum()
+}
+
+fn calc_steps(mut sequence: Vec<Vec<isize>>) -> Vec<Vec<isize>> {
+    let last = sequence.last().unwrap();
+    let mut steps = Vec::with_capacity(last.len() - 1);
+    for pair in last.windows(2) {
+        steps.push(pair[1] - pair[0]);
+    }
+    if steps.iter().all(|s| *s == 0) {
+        sequence
+    } else {
+        sequence.push(steps);
+        calc_steps(sequence)
+    }
+}
